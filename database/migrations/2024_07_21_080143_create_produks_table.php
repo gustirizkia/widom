eba7a42291b599edea6d8c76a3752c732cb65d3e
gustirizkia <gustidoang936@gmail.com>
2024-07-21 19:00:41 +0700

    add metronic

---
+++ database/migrations/2024_07_21_080143_create_produks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string("slug");
+            $table->string("nama");
+            $table->decimal("harga", 20, 2)->default(0);
+            $table->string("lebar")->nullable();
+            $table->string("panjang")->nullable();
+            $table->string("tinggi")->nullable();
+            $table->string("warna")->nullable();
+            $table->string("minimal_order")->nullable();
+            $table->string("material_plastik")->nullable();
+
+            $table->longText("kelengkapan")->nullable();
+            $table->longText("fitur")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
---
+++ database/seeders/KategoriProdukSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KategoriProdukSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $kategori = [
+            "Interior",
+            "Gift",
+            "Material",
+            "Dekorasi",
+            "Software",
+            "Plakat",
+            "Tools",
+            "Elektronik",
+            "Machine",
+            "Exterior",
+        ];
+
+        foreach($kategori as $item)
+        {
+            dd($item);
+        }
+
+    }
+}
